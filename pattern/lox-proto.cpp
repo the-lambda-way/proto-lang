e81c46558b942fbb30934d00953c2df27bad1768
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2019-10-01 11:32:58 -0500

    unexpected characters were stuck in a loop

--- pattern/lox-proto.cpp
+++ pattern/lox-proto.cpp
@@ -4,8 +4,8 @@
 // https://github.com/munificent/craftinginterpreters/tree/master/java/com/craftinginterpreters/lox
 
 
-#ifndef LOXPROTOTEST
-#define LOXPROTOTEST
+#ifndef LOXPROTO
+#define LOXPROTO
 
 #include "pattern2.cpp"
 
@@ -32,7 +32,7 @@ namespace LoxLang
 constexpr bool is_digit (char c)            { return '0' <= c && c <= '9'; }
 constexpr bool is_alpha (char c)            { return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_'; }
 constexpr bool is_alpha_numeric (char c)    { return is_alpha(c) || is_digit(c); }
-constexpr bool is_whitespace (char c)       { return c == ' ' || c == '\r' || c == '\t'; }
+constexpr bool is_whitespace (char c)       { return c == ' ' || c == '\t' || c == '\r'; }
 
 using namespace Scan;
 
@@ -131,6 +131,7 @@ std::string to_string (TokenType type) {
 
 template <typename T>
 std::string to_string (T val)              { return std::to_string(val); }
+std::string to_string (char c)             { return std::string(1, c); }
 std::string to_string (string_view s)      { return PL::to_string(s); }
 std::string to_string (std::nullptr_t)     { return "nullptr"; }
 
@@ -323,7 +324,11 @@ void Scanner::scan_token ()
         else if (is_digit(c))    number();
         else if (is_alpha(c))    identifier();
         else if (match("//"))    scan_while_not(view, '\n');    // is a comment
-        else                     error(line, "Unexpected character.\n");
+        else
+        {
+            error(line, "\"" + to_string(c) + "\", Unexpected character.\n");
+            advance();
+        }
     }
 }
 

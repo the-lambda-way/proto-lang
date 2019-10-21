e4640b397225292c8b47c5404b9c13280f97d32a
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2019-10-21 12:31:15 -0500

    removed range overloads, added sentinels

--- include/scanning-algorithms.h
+++ include/scanning-algorithms.h
@@ -18,22 +18,22 @@ concept bool char_predicate =
 // ------------------------
 // Predicates
 // ------------------------
-template <typename InputIt>
-constexpr bool starts_with (InputIt first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool starts_with (InputIt first, Sentinel last, char c)
 {
     return (first < last && *first == c);
 }
 
 
-template <typename InputIt>
-constexpr bool starts_with (InputIt first, InputIt last, char_predicate p)
+template <typename InputIt, typename Sentinel>
+constexpr bool starts_with (InputIt first, Sentinel last, char_predicate p)
 {
     return (first < last && p(*first));
 }
 
 
-template <typename InputIt>
-constexpr bool starts_with (InputIt first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel>
+constexpr bool starts_with (InputIt first, Sentinel last, string_view literal)
 {
     if constexpr (!starts_with(first, last, literal.front()))    return false;
     if constexpr (last - first > literal.length())               return false;
@@ -57,8 +57,8 @@ constexpr bool starts_with (Range r, T t)
 // ------------------------
 // Algorithms
 // ------------------------
-template <typename InputIt, typename T>
-constexpr bool advance_if (InputIt& first, InputIt last, T t)
+template <typename InputIt, typename Sentinel, typename T>
+constexpr bool advance_if (InputIt& first, Sentinel last, T t)
 {
     if (!starts_with(first, last, t))    return false;
     ++first;
@@ -66,8 +66,8 @@ constexpr bool advance_if (InputIt& first, InputIt last, T t)
 }
 
 
-template <typename InputIt>
-constexpr bool advance_if (InputIt& first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_if (InputIt& first, Sentinel last, string_view literal)
 {
     InputIt copy = first;
     if (!starts_with(&copy, last, literal))    return false;
@@ -77,23 +77,17 @@ constexpr bool advance_if (InputIt& first, InputIt last, string_view literal)
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_if (Range& r, T t)
-{
-    return advance_if(&r.begin(), r.end(), t);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_if_not (InputIt& first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_if_not (InputIt& first, Sentinel last, char c)
 {
     if (starts_with(first, last, c))    return false;
     ++first;
     return true;
 }
 
-template <typename InputIt>
-constexpr bool advance_if_not (InputIt& first, InputIt last, char_predicate p)
+
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_if_not (InputIt& first, Sentinel last, char_predicate p)
 {
     if (starts_with(first, last, p))    return false;
     ++first;
@@ -101,116 +95,76 @@ constexpr bool advance_if_not (InputIt& first, InputIt last, char_predicate p)
 }
 
 
-template <typename InputIt>
-constexpr bool advance_if_not (InputIt& first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_if_not (InputIt& first, Sentinel last, string_view literal)
 {
     if (starts_with(first, last, literal))    return false;
     ++first;
     return true;
 }
 
-template <typename Range, typename T>
-constexpr bool advance_if_not (Range& r, T t)
-{
-    return advance_if_not(&r.begin(), r.end(), t);
-}
-
 
 // Returns true even on no match
-template <typename InputIt, typename T>
-constexpr bool advance_optionally (InputIt& first, InputIt last, T t)
+template <typename InputIt, typename Sentinel, typename T>
+constexpr bool advance_optionally (InputIt& first, Sentinel last, T t)
 {
     advance_if(first, last, t);
     return true;
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_optionally (Range& r, T t)
-{
-    return advance_optionally(&r.begin(), r.end(), t);
-}
-
-
 // Returns true even on no match, like a Kleene star
-template <typename InputIt>
-constexpr bool advance_while (InputIt& first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_while (InputIt& first, Sentinel last, char c)
 {
     while (starts_with(first, last, c))    ++first;
     return true;
 }
 
 
-template <typename InputIt>
-constexpr bool advance_while (InputIt& first, InputIt last, char_predicate p)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_while (InputIt& first, Sentinel last, char_predicate p)
 {
     while (starts_with(first, last, p))    ++first;
     return true;
 }
 
 
-template <typename InputIt>
-constexpr bool advance_while (InputIt& first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_while (InputIt& first, Sentinel last, string_view literal)
 {
     while (advance_if(first, last, literal));
     return true;
 }
 
 
-
-
-// Ranges wont work for these, because they take the address of an iterator, and you cannot
-// assume that the range returns a reference from r.begin()
-// However, an overload for scan_view would work. Place in scan_view.h
-
-
-template <typename Range, typename T>
-constexpr bool advance_while (Range& r, T t)
-{
-    return advance_while(&r.begin(), r.end(), t);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_while_not (InputIt& first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_while_not (InputIt& first, Sentinel last, char c)
 {
     while (!starts_with(first, last, c))    ++first;
     return true;
 }
 
 
-template <typename InputIt>
-constexpr bool advance_while_not (InputIt& first, InputIt last, char_predicate p)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_while_not (InputIt& first, Sentinel last, char_predicate p)
 {
     while (!starts_with(first, last, p))    ++first;
     return true;
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_while_not (Range& r, T t)
-{
-    return advance_while_not(&r.begin(), r.end(), t);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_max_if (InputIt& first, InputIt last, char c, size_t max = -1)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_max_if (InputIt& first, Sentinel last, char c, size_t max = -1)
 {
     last = first + std::min(last - first, max);
     while (advance_if(first, last, c));
     return true;
 }
 
-template <typename Range, typename T>
-constexpr bool advance_max_if (Range& r, T t, size_t max = -1)
-{
-    return advance_max_if(&r.begin(), r.end(), t, max);
-}
 
-
-template <typename InputIt>
-constexpr bool advance_n_if (InputIt& first, InputIt last, char c, size_t n)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_n_if (InputIt& first, Sentinel last, char c, size_t n)
 {
     if (first - last < n)    return false;
     if (n == 0)              return true;
@@ -226,16 +180,9 @@ constexpr bool advance_n_if (InputIt& first, InputIt last, char c, size_t n)
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_n_if (Range& r, T t, size_t n)
-{
-    return advance_n_if(&r.begin(), r.end(), t, n);
-}
-
-
 // Probably needs an optional return value
-template <typename InputIt>
-constexpr bool advance_min_if (InputIt& first, InputIt last, char c, size_t min = 0)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_min_if (InputIt& first, Sentinel last, char c, size_t min = 0)
 {
     if (!advance_n_if(first, last, c, min))    return false;
     advance_while(first, last, c);
@@ -243,15 +190,8 @@ constexpr bool advance_min_if (InputIt& first, InputIt last, char c, size_t min 
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_min_if (Range& r, T t, size_t min = 0)
-{
-    return advance_min_if(&r.begin(), r.end(), t, min);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_repeating (InputIt& first, InputIt last, char c,
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_repeating (InputIt& first, Sentinel last, char c,
                                   size_t min = 0, size_t max = -1)
 {
     if (max < min)    return false;
@@ -262,15 +202,8 @@ constexpr bool advance_repeating (InputIt& first, InputIt last, char c,
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_min_if (Range& r, T&& t, size_t min = 0, size_t max = -1)
-{
-    return advance_min_if(&r.begin(), r.end(), t, min, max);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_to_if_found (InputIt& first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_to_if_found (InputIt& first, Sentinel last, char c)
 {
     auto it = std::find(first, last, c);
     if (it == last)    return false;
@@ -280,8 +213,8 @@ constexpr bool advance_to_if_found (InputIt& first, InputIt last, char c)
 }
 
 
-template <typename InputIt>
-constexpr bool advance_to_if_found (InputIt& first, InputIt last, char_predicate p)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_to_if_found (InputIt& first, Sentinel last, char_predicate p)
 {
     auto it = std::find_if(first, last, p);
     if (it == last)    return false;
@@ -291,8 +224,8 @@ constexpr bool advance_to_if_found (InputIt& first, InputIt last, char_predicate
 }
 
 
-template <typename InputIt, typename T>
-constexpr bool advance_to_if_found (InputIt& first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel, typename T>
+constexpr bool advance_to_if_found (InputIt& first, Sentinel last, string_view literal)
 {
     const T searcher = std::boyer_moore_searcher(literal.begin(), literal.end());
     auto it = searcher(first, last).first;
@@ -304,26 +237,19 @@ constexpr bool advance_to_if_found (InputIt& first, InputIt last, string_view li
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_to_if_found (Range r, T t)
-{
-    return advance_if_found(r.begin(), r.end(), t);
-}
-
-
-template <typename InputIt>
-constexpr bool advance_past_if_found (InputIt& first, InputIt last, char c)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_past_if_found (InputIt& first, Sentinel last, char c)
 {
     auto it = std::find(first, last, c);
     if (it == last)    return false;
 
     first = it + 1;
-    return true;;
+    return true;
 }
 
 
-template <typename InputIt>
-constexpr bool advance_past_if_found (InputIt& first, InputIt last, char_predicate p)
+template <typename InputIt, typename Sentinel>
+constexpr bool advance_past_if_found (InputIt& first, Sentinel last, char_predicate p)
 {
     auto it = std::find_if(first, last, p);
     if (it == last)    return false;
@@ -333,8 +259,8 @@ constexpr bool advance_past_if_found (InputIt& first, InputIt last, char_predica
 }
 
 
-template <typename InputIt, typename T>
-constexpr bool advance_past_if_found (InputIt& first, InputIt last, string_view literal)
+template <typename InputIt, typename Sentinel, typename T>
+constexpr bool advance_past_if_found (InputIt& first, Sentinel last, string_view literal)
 {
     const T searcher = std::boyer_moore_searcher(literal.begin(), literal.end());
     auto it = searcher(first, last).second;
@@ -346,29 +272,15 @@ constexpr bool advance_past_if_found (InputIt& first, InputIt last, string_view 
 }
 
 
-template <typename Range, typename T>
-constexpr bool advance_past_if_found (Range& r, T t)
-{
-    return advance_past_if_found(&r.begin(), r.end(), t);
-}
-
-
-template <typename InputIt, typename... Exp>
-constexpr bool advance_if_any (InputIt& first, InputIt last, Exp... e)
+template <typename InputIt, typename Sentinel, typename... Exp>
+constexpr bool advance_if_any (InputIt& first, Sentinel last, Exp... e)
 {
     return (... || e(first, last));
 }
 
 
-template <typename Range, typename... Exp>
-constexpr bool advance_if_any (Range& r, Exp... e)
-{
-    return advance_if_any(&r.begin(), r.end(), e...);
-}
-
-
-template <typename InputIt, typename... Exp>
-constexpr bool advance_joined_if (InputIt& first, InputIt last, Exp... e)
+template <typename InputIt, typename Sentinel, typename... Exp>
+constexpr bool advance_joined_if (InputIt& first, Sentinel last, Exp... e)
 {
     InputIt copy = first;
 
@@ -376,13 +288,6 @@ constexpr bool advance_joined_if (InputIt& first, InputIt last, Exp... e)
 
     first = copy;
     return true;
-}
-
-
-template <typename Range, typename... Exp>
-constexpr bool advance_joined_if (Range& r, Exp... e)
-{
-    return advance_joined(&r.begin(), r.end(), e...);
 }
 
 

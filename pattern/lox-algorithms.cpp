c1a223e69106c80fa85cbddabd72b4029c665800
the-lambda-way <32426282+the-lambda-way@users.noreply.github.com>
2019-10-22 01:30:32 -0500

    update

--- pattern/lox-algorithms.cpp
+++ pattern/lox-algorithms.cpp
@@ -8,8 +8,8 @@
 #include <string>
 #include <string_view>
 #include <vector>      // token list
-#include "scan_view.h"
 #include "../include/syntax.h"
+#include "scan_view.h"
 #include "scanning-algorithms.h"
 #include "lox-common.h"
 
